3425e7199b633926c524aaadb248127322d6cd69
PLarnaudieGlobant <pablo.larnaudie@globant.com>
2024-10-09 12:08:03 -0300

    avanzando

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -29,3 +29,38 @@ Then (/^URL should match (.*)$/,async function (expectedUrl) {
     let url = await browser.getUrl();
     expect(url).to.equal(expectedUrl);
 })
+
+Given ("A  web page is opened", async function () {
+    await browser.url("/inputs");
+    await browser.setTimeout({implicit: 15000, pageLoad: 10000,});
+    await browser.maximizeWindow();
+
+})
+
+
+When  ("Perform web Interactions", async function () {
+    let selector = await $(`input[type="number"]`);
+    // Hacemos click en el selector
+    await selector.click();
+    // Escribir un valor en el input
+    await selector.setValue("1234");
+    // await browser.debug();
+    // await browser.pause(3000);
+
+    //Escribir lento
+    let numero = 12345;
+    let numeroString = numero.toString();
+    for (let i = 0; i < numeroString.length; i++) {
+        let currentElement = numeroString.charAt(i);
+        await browser.pause(2000);
+        //simula la forma que tenemos de hacer enter.
+        await browser.keys(currentElement);
+    }
+
+    //implementamos el moveTo o scrollIntoView()
+    await browser.url("");
+    let selector2 = await $(`a[href="/tinymce"]`);
+    // await selector2.moveTo();
+    await selector2.scrollIntoView()
+
+})
