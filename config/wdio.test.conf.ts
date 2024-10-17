eedcdf0004915c979a72b0911da8dbc614ff64a9
PLarnaudieGlobant <pablo.larnaudie@globant.com>
2024-10-17 20:59:29 -0300

    yaml

--- config/wdio.test.conf.ts
+++ config/wdio.test.conf.ts
@@ -1,4 +1,5 @@
 import { config as baseConfig } from '../wdio.conf';
+require('dotenv').config();
 export const config = Object.assign(baseConfig, {
     //Configuracion del ambiente nuevo
     environment: "TEST",
