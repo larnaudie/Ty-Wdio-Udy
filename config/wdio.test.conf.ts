67ed5f03ca46810a2ae283e8d510930da44b0f0b
PLarnaudieGlobant <pablo.larnaudie@globant.com>
2024-10-16 23:56:55 -0300

    avances seccion 15

--- config/wdio.test.conf.ts
+++ config/wdio.test.conf.ts
@@ -1,7 +1,7 @@
 import { config as baseConfig } from '../wdio.conf';
 export const config = Object.assign(baseConfig, {
     //Configuracion del ambiente nuevo
-    environment: "Test",
+    environment: "TEST",
     sauceDemoURL: "https://saucedemo.com"
 })
 
